29c737da4ef6c679136172bf2b4c470320f0dded
szq <380846595@qq.com>
2020-09-16 11:47:23 +0800

    test

---
+++ build/precache-manifest.951bdfa0b95b7631864fc56a186bb973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6986a02f6181f36ae01fbcc22ad04ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c130b6917dae42e724c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6be8f8d5f50340c994f",
+    "url": "/static/js/2.47c71f1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47c71f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c130b6917dae42e724c",
+    "url": "/static/js/main.7837213b.chunk.js"
+  },
+  {
+    "revision": "142d6829b916f62f2bb9",
+    "url": "/static/js/runtime-main.046262a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
